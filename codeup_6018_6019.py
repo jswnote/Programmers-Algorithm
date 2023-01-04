702d118908311327079229790dd2a9e6fc4bc6e9
jungsw_spi <105158914+jsw2231020006@users.noreply.github.com>
2023-01-04 14:40:34 +0900

    update

---
+++ codeup_6018_6019.py
@@ -0,0 +1,18 @@
+'''
+6018-시간입출력
+a, b = input().split(':')
+
+print(a, b, sep=':')
+
+#sep은 분류기호를 의미.
+'''
+
+'''
+#6019-년원일
+y,m,d = input().split('.')
+print(d,m,y,sep='-')
+'''
+
+#6020-주민등록번호
+a,b = input().split('-')
+print(a,b,sep='')
---
+++ codeup_6020.py
@@ -0,0 +1,3 @@
+#6020-주민등록번호
+a,b = input().split('-')
+print(a,b,sep='')
