d16dfe10c4cea72796dd97e573a0e23eea4303be
jungsw_spi <jsw1995326@naver.com>
2023-01-18 15:55:15 +0900

    update

--- greedy-3-2_큰수의법칙.py
+++ greedy-3-2_큰수의법칙.py
@@ -7,7 +7,28 @@
 첫쨰 줄에 N(2~1000), M(1~10000), K(1~10000)의 자연수가 주어지며, 각 자연수는 공백으로 구분한다.
 둘째 줄에 N개의 자연수가 주어진다. 각 자연수는 공백으로 구분한다. 단 각각의 자연수는 1 이상 10000 이하의 수로 주어진다.
 입력으로 주어지는 K는 항상 M보다 작거나 같다.
+
+입력
+5 8 3
+2 4 5 4 6
+
+출력 46 6665666
 '''
 
-N,M,K = map(int(), input().split())
+N,M,K = map(int, input().split())
+
+N_list = list(map(int,input().split()))
+
+N_list.sort()
+
+total = 0
+
+for i in range(M):
+    if i % (K+1) != 0:
+        total += N_list[N-1]
+    elif i % (K+1) == 0:
+        total += N_list[N-2]
+    
+
+print(total)
 
