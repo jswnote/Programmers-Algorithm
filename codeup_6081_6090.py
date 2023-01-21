989957ac8cf1f31f26d5e8dffd5d68dd9d97fca0
jungsw_spi <jsw1995326@naver.com>
2023-01-21 23:30:25 +0900

    update

---
+++ codeup_6081_6090.py
@@ -0,0 +1,61 @@
+'''
+6082번 - 3,6,9 게임의 왕이 되자
+a = int(input())
+
+for i in range(1, a+1):
+    if i % 10 == 3 or i % 10 == 6 or i % 10 == 9:
+        print('X', end = ' ')
+          
+    else:
+        print(i, end = ' ')
+
+'''
+
+'''
+6083번 - 빛 섞어 색 만들기
+a,b,c = map(int,input().split())
+count = 0 
+
+for i in range(a):
+    for j in range(b):
+        for k in range(c):
+            print(i,j,k, sep = ' ')
+            count += 1
+            
+print(count)
+'''
+
+'''
+6087번 - 3의 배수는 통과
+a = int(input())
+for i in range(1, a+1):
+    if i % 3 == 0:
+        pass
+    else:
+        print(i, end = ' ')
+'''
+
+'''
+6088번 - 수 나열하기1
+a,b,c = map(int,input().split())
+
+d = a + (c-1)*b
+print(d)
+'''
+
+'''
+6089번 - 수 나열하기2
+a,b,c = map(int,input().split())
+d = (a*(b**(c - 1)))
+print(d)
+'''
+
+'''
+6090번 - 수 나열하기3
+a, b, c, d = map(int,input().split())
+
+for i in range(1, d):
+    a = (a * b) + c
+    
+print(a)
+'''
--- 연습장.py
+++ 연습장.py
@@ -1,5 +1,5 @@
 #n = list(input().split())
 #print(n)
 
-for i in range(5):
+for i in range(1,5):
     print(i)
