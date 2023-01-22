e8f0ccec43c9bb89f55ad2a0068100398f78ee49
jungsw_spi <jsw1995326@naver.com>
2023-01-22 23:25:46 +0900

    update

--- codeup_6081_6090.py
+++ codeup_6081_6090.py
@@ -1,4 +1,11 @@
 '''
+6081번 - 16진수 구구단 출력하기
+a = int(input(),16)
+for i in range(1,16):
+  print ('%X*%X=%X' %(a,i,a*i))
+'''
+
+'''
 6082번 - 3,6,9 게임의 왕이 되자
 a = int(input())
 
@@ -26,12 +33,35 @@ print(count)
 '''
 
 '''
+6084번 - 소리 파일 저장용량 계산하기
+h, b, c, s = map(int, input().split())
+e = h*b*c*s/8/1024/1024
+print('%.1f'%e,'MB')
+'''
+
+'''
 6085번 - 그림 파일 저장용량 계산하기
 a,b,c = map(int,input().split())
 
 d = a*b*c/8/1024/1024
 
 print('%.2f'%d,"MB")
+'''
+
+'''
+6086번 - 거기까지! 이제 그만~
+a = int(input())
+
+count = 0 
+next = 0
+while True:
+    next += 1
+    count += next
+    
+    if count >= a:
+        break
+
+print(count)
 '''
 
 '''
