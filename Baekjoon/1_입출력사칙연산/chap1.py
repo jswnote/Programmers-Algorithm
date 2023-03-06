96ba2e88dc8f69c4942ab6ed1e75b93c5bcbfc95
jungsw_spi <jsw1995326@naver.com>
2023-03-06 13:22:41 +0900

    update

---
+++ Baekjoon/1_입출력사칙연산/chap1.py
@@ -0,0 +1,107 @@
+'''
+2557번 - Hello World
+print('Hello Wolrd!')
+'''
+
+'''
+1000번 - A+B
+a,b = map(int, input().split())
+c = a+b
+
+print(c)
+'''
+
+'''
+1001번 - A-B
+a,b = map(int,input().split())
+c = a-b
+
+print(c)
+'''
+
+'''
+10998번 - AxB
+a,b = map(int,input().split())
+
+c = a*b
+print(c)
+'''
+
+'''
+1008번 - A/B
+a,b = map(int,input().split())
+
+c = a/b
+print(c)
+'''
+
+'''
+10869번 - 사칙연산
+a,b = map(int,input().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+'''
+
+'''
+10926번 - ??!
+a = input()
+print(a+'??!')
+'''
+
+'''
+18108번 - 1998년생인 내가 태국에서는 2541년생?!
+a = int(input())
+
+pritn(a-543)
+'''
+
+'''
+10430번 - 나머지
+a,b,c, = map(int, input().split())
+print((a+b)%c)
+print(((a%c)+(b%c))%c)
+print((a*b)%c)
+print(((a%c)*(b%c))%c)
+'''
+
+'''
+2588번 - 곱셈
+a = int(input())
+b = int(input())
+c = b//100 #몫 3
+d = b%100 #나머지 85
+e = d//10 #목 8
+f = d%10
+
+print(a*f)
+print(a*e)
+print(a*c)
+print(a*b)
+'''
+
+'''
+11382번 - 꼬마정민
+a,b,c = map(int,input().split())
+
+print(a+b+c)
+'''
+
+'''
+10171번 - 고양이
+print("\\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \\(__)|")
+'''
+
+'''
+10172 - 개
+print("|\_/|")
+print("|q p|   /}")
+print('( 0 )"""\\')  
+print('|"^"`    |')
+print("||_/=\\\__|") 
+'''
--- 연습장.py
+++ 연습장.py
@@ -60,3 +60,18 @@ print(len(total))
 
 print()
 '''
+
+
+
+graph = [[],[2,3,8],[1,7],[1,4,5],[3,5],[3,4],[7],[2,6,8],[1,7]]
+
+for i in graph[1]:
+    print(i)
+
+
+'''
+visited = [False] * 9
+
+print(visited)
+'''
+
